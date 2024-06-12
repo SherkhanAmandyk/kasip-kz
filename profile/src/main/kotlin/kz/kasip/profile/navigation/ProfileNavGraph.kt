6ce1ad2d923a2db12e77ba19feec6b4bb224a58b
SherkhanAmandyk <91122561+sherkhanamandyk@users.noreply.github.com>
2024-06-12 18:32:44 +0500

    bug fixes

--- profile/src/main/kotlin/kz/kasip/profile/navigation/ProfileNavGraph.kt
+++ profile/src/main/kotlin/kz/kasip/profile/navigation/ProfileNavGraph.kt
@@ -3,6 +3,7 @@ package kz.kasip.profile.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import kz.kasip.profile.ui.changebio.ChangeBioScreen
+import kz.kasip.profile.ui.changebio.ChangeCountryScreen
 import kz.kasip.profile.ui.changecity.ChangeCityScreen
 import kz.kasip.profile.ui.changename.ChangeNameScreen
 import kz.kasip.profile.ui.changespeciality.ChangeSpecialityScreen
@@ -44,5 +45,8 @@ fun NavGraphBuilder.profileNavGraph(
         ChangeCityScreen(
             onBack = onBack
         )
+    }
+    composable(changeCountryScreen) {
+        ChangeCountryScreen(onBack = onBack)
     }
 }
